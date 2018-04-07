88a98ef617b1753b12b8821d85a06907f7df6fdd
Maxi Clayton Clowes <maxi@codogo.io>
2018-04-07 13:26:24 +0800

    fix(velocity): Expected window - fixed??

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,15 @@ var path = require('path');
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  if (stage === 'build-html') {
+    config.loader('null', {
+      test: /velocity/,
+      loader: 'null-loader'
+    })
+  }
+}
+
  // You can delete this file if you're not using it
 
  // Implement the Gatsby API “createPages”. This is called once the
