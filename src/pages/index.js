0c55ca17d63986f908a32c26c7a59feb485cc8ea
Maxi Clayton Clowes <maxi@codogo.io>
2018-09-11 00:32:50 +0100

    [feat] news added to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,20 @@ import PropTypes from "prop-types";
 
 export const HomeQuery = graphql`
 	query HomeQuery {
-		contentfulNews(limit: 2)  {
-			id
-			title
-			description {
-				description
-			}
-			publishingDate
-			newsSection
-			image {
-				file {
-					url
+		allContentfulNews {
+			edges {
+				node {
+					title
+					description {
+						description
+					}
+					publishingDate
+					newsSection
+					image {
+						file {
+							url
+						}
+					}
 				}
 			}
 		}
@@ -28,74 +31,96 @@ export const HomeQuery = graphql`
 
 // ----------------------------------------------------
 
-const Home = ( { data, }, ) => (
-	<div>
-		<Segment style = { { padding: "8em 0em", } } vertical>
-			<Container text>
-				<Header as = "h1" style = { { fontSize: "2em", } }>
-					For the many, not the few.
-				</Header>
+const Home = ( { data, }, ) => {
+	let news = data.allContentfulNews && data.allContentfulNews.edges;
 
-				<p style = { { fontSize: "1.33em", } }>
-					We want to build a Britain that works for the many, not the
-					few. That means building the homes we need to rent and buy,
-					keeping our communities safe, giving our schools the funding
-					they need, and restoring the NHS to its place as the envy of
-					the world.
-				</p>
+	news = news.map( entry => {
+		return entry.node || entry;
+	});
 
-				<Button
-					size = "huge"
-					as = { Link }
-					to = "/news/Haringey-Labour-2018-Manifesto-Launched"
-				>
-					Read our manifesto
-				</Button>
-			</Container>
-		</Segment>
+	if (news[0].publishingDate) {
+		news
+			.sort(function(a, b) {
+				return (
+					new Date(b.publishingDate) -
+					new Date(a.publishingDate)
+				);
+			});
+	};
 
-		<Segment style = { { padding: "8em 0em", } } vertical>
-			<Container text>
-				<Header as = "h1" style = { { fontSize: "2em", } }>
-					Meet your councillors
-				</Header>
+	return (
+		<div>
+			<Segment style = { { padding: "8em 0em", } } vertical>
+				<Container text>
+					<Header as = "h1" style = { { fontSize: "2em", } }>
+						For the many, not the few.
+					</Header>
 
-				<p style = { { fontSize: "1.33em", } }>
-					There are 15 Labour councillors representing you across
-					Hornsey & Wood Green. Find out who is representing your
-					local area.
-				</p>
+					<p style = { { fontSize: "1.33em", } }>
+						We want to build a Britain that works for the many, not the
+						few. That means building the homes we need to rent and buy,
+						keeping our communities safe, giving our schools the funding
+						they need, and restoring the NHS to its place as the envy of
+						the world.
+					</p>
 
-				<Button size = "huge" as = { Link } to = "/councillors">
-					Meet your councillors
-				</Button>
-			</Container>
-		</Segment>
+					<Button
+						size = "huge"
+						as = { Link }
+						to = "/news/Haringey-Labour-2018-Manifesto-Launched"
+					>
+						Read our manifesto
+					</Button>
+				</Container>
+			</Segment>
 
-		<Segment style = { { padding: "8em 0em", } } vertical>
-			<Container text>
-				<Header as = "h3" style = { { fontSize: "2em", } }>
-					Recent News
-				</Header>
+			<Segment style = { { padding: "8em 0em", } } vertical>
+				<Container text>
+					<Header as = "h1" style = { { fontSize: "2em", } }>
+						Meet your councillors
+					</Header>
 
-				<br/>
-				<br/>
+					<p style = { { fontSize: "1.33em", } }>
+						There are 15 Labour councillors representing you across
+						Hornsey & Wood Green. Find out who is representing your
+						local area.
+					</p>
 
-				{data.contentfulNews.length && (
-					<Grid columns = { 2 } stackable>
-						{data.contentfulNews.map(newsItem => {
-							return <NewsItem news = { newsItem.node } />;
-						})}
-					</Grid>
-				)}
-			</Container>
-		</Segment>
-	</div>
-);
+					<Button size = "huge" as = { Link } to = "/councillors">
+						Meet your councillors
+					</Button>
+				</Container>
+			</Segment>
+
+			<Segment style = { { padding: "8em 0em", } } vertical>
+				<Container text>
+					<Header as = "h3" style = { { fontSize: "2em", } }>
+						Recent News
+					</Header>
+
+					<br/>
+					<br/>
+
+					{news.length && (
+						<Grid columns = { 2 } stackable>
+							{news.slice(0,2).map(newsItem => {
+								return <NewsItem news = { newsItem } />;
+							})}
+						</Grid>
+					)}
+
+					<Button size = "huge" as = { Link } to = "/news">
+						Read more news
+					</Button>
+				</Container>
+			</Segment>
+		</div>
+	)
+};
 
 Home.propTypes = {
 	data: PropTypes.shape({
-		contentfulNews: PropTypes.object,
+		allContentfulNews: PropTypes.object,
 	}),
 };
 
